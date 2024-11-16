da171917be5caf09df2a14a92150566df496f9cd
Bagas Bawazir <bawazirfadhil@gmail.com>
2024-11-16 22:12:36 +0700

    update code bagasbawazir

--- database/seeders/FakultasSeeder.php
+++ database/seeders/FakultasSeeder.php
@@ -14,7 +14,7 @@ class FakultasSeeder extends Seeder
      */
     public function run()
     {
-        $fakultas = collect(['teknologi informasi','sistem informasi','rekayasa perangkat lunak','broadcasting','ilmu komputer','teknik komputer jaringan','sistem database','game development', 'machine learning' ,'cloud sistem', 'multimedia' ]);
+        $fakultas = collect(['teknologi informasi','sistem informasi','rekayasa perangkat lunak','ilmu komputer','teknik komputer jaringan','sistem database','game development', 'machine learning' ,'cloud sistem' ]);
 
         $fakultas->each(function($fk){
             $arr = explode(' ', $fk);
